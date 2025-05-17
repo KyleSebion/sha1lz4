dea5884253f73094d736fe49be42c53b27a8942d
KyleSebion <kyle@kylesebion.com>
2025-05-16 21:40:08 -0500

    Add dependencies for itertools and either; implement SHA1 file hashing in main function; create launch configuration for debugging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, io::{Error, Read}};
+use sha1::{digest::generic_array::functional::FunctionalSequence, Digest, Sha1};
+
+fn main() -> Result<(), Error> {
+    let mut h = Sha1::new();
+    h.update(b"00");
+    println!("{}", h.finalize().map(|b| format!("{b:02x}")).join(""));
+
+
+    let mut f = File::open("C:\\a.txt")?;
+    let mut b =  [0u8; 256 * 1024];
+    let mut oh = Sha1::new();
+    loop {
+        let l = f.read(&mut b)?;
+        if l == 0 { break; }
+        oh.update(&b[0..l]);
+    }
+    println!("{}", oh.finalize().map(|b| format!("{b:02x}")).join(""));
+    Ok(())
 }
